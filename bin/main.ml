586a72640f2b7627228f6c98e2e8a6d6a9eae3df
Femi Agbabiaka <femi@femiagbabiaka.xyz>
2018-06-13 22:55:35 -0700

    Account for missing CLI argument.

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,8 @@
 open Test;;
 
-let () =  
-  Test.say_hello Sys.argv.(1)
+let name =
+  if Sys.argv |> Array.length > 1 then Sys.argv.(1) else "World"
+
+let () =
+  Test.say_hello name
   |> print_endline;;
